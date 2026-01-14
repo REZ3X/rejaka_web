79344fc400903f6281cf21b82cb63435f2213451
REZ3X <rezex.dev01@gmail.com>
2026-01-14 09:40:40 +0700

    feat(projects): add Snaplove Digital Photobooth project details and features

--- src/app/api/data/projects/route.ts
+++ src/app/api/data/projects/route.ts
@@ -1,3 +1,4 @@
+import { link } from "fs";
 import { NextResponse } from "next/server";
 import { title } from "process";
 
@@ -41,6 +42,32 @@ export async function GET(request: Request) {
         //   url: "https://github.com/REZ3X/alimentify_backend",
         //   type: "url",
         // }
+      ],
+    },
+    {
+      id: "snaplove_digital_photobooth",
+      title: "Snaplove Digital Photobooth",
+      subtitle: "A digital photobooth service for everyone",
+      category: "web",
+      description: [
+        "Snaplove Digital Photobooth is a web-based application that allows users to capture and share memorable moments through a digital photobooth experience. The platform is designed to be user-friendly and accessible, making it easy for anyone to create fun and engaging photos.",
+        "Sharing frames and effects to enhance the photobooth experience.",
+      ],
+      // technologies: ["Next.js", "MongoDB", "JavaScript", "Tailwind CSS"],
+      features: [
+        "Digital Photobooth: Capture photos using a web-based photobooth interface",
+        "Customizable Frames and Effects: Enhance photos with a variety of frames and effects",
+        "Frame Sharing: Easily upload and share customized frames with others",
+        "AI Photo Partner: Utilize AI technology to bring your fictional photo partner to life",
+        "Live Photo: Dynamic photo generation for a unique experience",
+      ],
+      year: 2025,
+      links: [
+        {
+          label: "Live Website",
+          url: "https://snaplove.pics",
+          type: "url",
+        },
       ],
     },
     {
